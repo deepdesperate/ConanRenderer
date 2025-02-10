a5f6da399e218175115d86d27a96f2eade75fbf5
Naman Deep <namandeep444@gmail.com>
2025-02-10 22:29:07 +0530

    UV projection and loading png images

--- src/main.c
+++ src/main.c
@@ -2,25 +2,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include "upng.h"
 #include "array.h"
 #include "display.h"
 #include "vector.h"
 #include "matrix.h"
+#include "light.h"
 #include "mesh.h"
+#include "texture.h"
+#include "triangle.h"
 
 triangle_t* triangles_to_render = NULL;
 
 vec3_t camera_position = { 0, 0, 0 };
 vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0};
 
-float fov_factor = 640;
-
 bool is_running = false;
 int previous_frame_time = 0;
 
+mat4_t proj_matrix;
+
 void setup(void) {
     // Initialize render mode and triangle culling method
-    render_method = RENDER_WIRE;
+    render_method = RENDER_TEXTURED_WIRE;
     cull_method = CULL_BACKFACE;
 
     // Allocate the required memory in bytes to hold the color buffer
@@ -29,16 +33,26 @@ void setup(void) {
     // Creating a SDL texture that is used to display the color buffer
     color_buffer_texture = SDL_CreateTexture(
         renderer,
-        SDL_PIXELFORMAT_ARGB8888,
+        SDL_PIXELFORMAT_RGBA32,
         SDL_TEXTUREACCESS_STREAMING,
         window_width,
         window_height
     );
 
+    // Initialize the perspective projection matrix
+    float fov = M_PI / 3.0;
+    float aspect = (float)window_height / (float)window_width;
+    float znear = 0.1;
+    float zfar = 100.0;
+    proj_matrix = mat4_make_perspective(fov, aspect, znear, zfar);
+
     // Loads the cub values in our mesh data structure
-    load_cube_mesh_data();
-    // load_obj_file_data("./assets/cube.obj");
+    // load_cube_mesh_data();
+    load_obj_file_data("./assets/cube.obj");
     // load_obj_file_data("./assets/f22.obj");
+
+    // Load the texture information from a png file
+    load_png_texture_data("./assets/cube.png");
 
 }
 
@@ -61,6 +75,10 @@ void process_input(void) {
                 render_method = RENDER_FILL_TRIANGLE;
             if (event.key.keysym.sym == SDLK_4)
                 render_method = RENDER_FILL_TRIANGLE_WIRE;
+            if (event.key.keysym.sym == SDLK_5)
+                render_method = RENDER_TEXTURED;
+            if (event.key.keysym.sym == SDLK_6)
+                render_method = RENDER_TEXTURED_WIRE;
             if (event.key.keysym.sym == SDLK_c)
                 cull_method = CULL_BACKFACE;
             if (event.key.keysym.sym == SDLK_d)
@@ -69,15 +87,6 @@ void process_input(void) {
     }
 }
 
-// Function that recieves a 3D vector and returns a projected 2D point
-
-vec2_t project(vec3_t point){
-    vec2_t projected_point = {
-        .x = (fov_factor * point.x) / point.z,
-        .y = (fov_factor * point.y) / point.z
-    };
-    return projected_point;
-}
 
 void update(void){
     // while (SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME))
@@ -88,12 +97,9 @@ void update(void){
     triangles_to_render = NULL;
 
     mesh.rotation.x += 0.001;
-    mesh.rotation.y += 0.001;
-    mesh.rotation.z += 0.001;
+    // mesh.rotation.y += 0.01;
+    // mesh.rotation.z += 0.01;
 
-    // mesh.scale.x += 0.002;
-    // mesh.scale.y += 0.001;
-    mesh.translation.x += 0.001;
     mesh.translation.z = 5.0;
 
 
@@ -120,69 +126,92 @@ void update(void){
         for( int j = 0; j < 3; j++ ){
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-            // Use a matrix to scale our original vertex
-            transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+            // Create a World Matrix combining scale, rotation and translation matrices
+            // [L] * [R] * [S] * [Identity ] = [World_matrix]
+
+            mat4_t world_matrix = mat4_identity();
+            world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+            world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
+            // Multiply the world matrix by the original vector
+            transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
             // Save transformed vertex in the array of transformed vertices
             transformed_vertices[j] = transformed_vertex;
         }
 
         // Backface culling condition
+        // Check backfaces culling
+        vec3_t vector_a = vec3_from_vec4(transformed_vertices[0]);  /*   A   */
+        vec3_t vector_b = vec3_from_vec4(transformed_vertices[1]);  /*  / \  */
+        vec3_t vector_c = vec3_from_vec4(transformed_vertices[2]);  /* C---B */
+
+        vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+        vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+        vec3_normalize(&vector_ab);
+        vec3_normalize(&vector_ac);
+
+        // Compute the face normal (using cross product to find perpendicular )
+        vec3_t normal = vec3_cross(vector_ab, vector_ac);
+        vec3_normalize(&normal);
+
+        // Find the vector between a point in the triangle and the camera origin
+        vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+
+        // Calculate how aligned the camera ray is with the face normal (using dot product)
+        float dot_normal_camera = vec3_dot(normal, camera_ray);
+
+        // Bypass the triangle that are looking away from the camera
         if (cull_method == CULL_BACKFACE){
-            // Check backfaces culling
-            vec3_t vector_a = vec3_from_vec4(transformed_vertices[0]);  /*   A   */
-            vec3_t vector_b = vec3_from_vec4(transformed_vertices[1]);  /*  / \  */
-            vec3_t vector_c = vec3_from_vec4(transformed_vertices[2]);  /* C---B */
-
-            vec3_t vector_ab = vec3_sub(vector_b, vector_a);
-            vec3_t vector_ac = vec3_sub(vector_c, vector_a);
-            vec3_normalize(&vector_ab);
-            vec3_normalize(&vector_ac);
-
-            // Compute the face normal (using cross product to find perpendicular )
-            vec3_t normal = vec3_cross(vector_ab, vector_ac);
-            vec3_normalize(&normal);
-
-            // Find the vector between a point in the triangle and the camera origin
-            vec3_t camera_ray = vec3_sub(camera_position, vector_a);
-
-            // Calculate how aligned the camera ray is with the face normal (using dot product)
-            float dot_normal_camera = vec3_dot(normal, camera_ray);
-
-            // Bypass the triangle that are looking away from the camera
             if ( dot_normal_camera < 0 ) {
                 continue;
             }
         }
 
 
-        vec2_t projected_ponts[3];
+        vec4_t projected_points[3];
         // Loop all three vertices to perform projection
         for (int j = 0; j < 3; j++){
             // Project the current vertex
-            projected_ponts[j] = project(vec3_from_vec4(transformed_vertices[j]));
+            projected_points[j] = mat4_mul_vec4_project(proj_matrix, transformed_vertices[j]);
 
-            // Scale and translate the projected point to the center of the screen
-            projected_ponts[j].x += (window_width / 2);
-            projected_ponts[j].y += (window_height / 2);
+            projected_points[j].y *= -1;
+
+            // Scale into the view
+            projected_points[j].x *= (window_width / 2.0);
+            projected_points[j].y *= (window_height / 2.0);
+
+            // Translate the projected point to the center of the screen
+            projected_points[j].x += (window_width / 2.0);
+            projected_points[j].y += (window_height / 2.0);
+            
         }
 
         // Calculate the average depth for each face based on the vertices after transformation
         float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z +transformed_vertices[2].z) / 3.0;
         
+        // -ve Cuz of light direction
+        float light_intensity_factor = -vec3_dot(normal, light.direction);
+
+        // Calculate the triangle color based on how aligned is the normal and the inverse of the light ray
+        uint32_t triangle_color = light_apply_intensity(mesh_face.color, light_intensity_factor);
+
         triangle_t projected_triangle = {
             .points = {
-                {projected_ponts[0].x, projected_ponts[0].y},
-                {projected_ponts[1].x, projected_ponts[1].y},
-                {projected_ponts[2].x, projected_ponts[2].y},
+                {projected_points[0].x, projected_points[0].y, projected_points[0].z, projected_points[0].w},
+                {projected_points[1].x, projected_points[1].y, projected_points[1].z, projected_points[1].w},
+                {projected_points[2].x, projected_points[2].y, projected_points[2].z, projected_points[2].w},
             },
-            .color = mesh_face.color,
+            .texcoords = {
+                { mesh_face.a_uv.u, mesh_face.a_uv.v },
+                { mesh_face.b_uv.u, mesh_face.b_uv.v },
+                { mesh_face.c_uv.u, mesh_face.c_uv.v }
+            },
+            .color = triangle_color,
             .avg_depth = avg_depth
-            // TODO: Avg depth per triangle
         };
 
         // Save the projected triangle in the array of triangles to render
@@ -224,8 +253,18 @@ void render(void){
             );
         }
 
+        // Draw textured triangle
+        if (render_method == RENDER_TEXTURED || render_method == RENDER_TEXTURED_WIRE) {
+            draw_textured_triangle(
+                triangle.points[0].x, triangle.points[0].y, triangle.points[0].z, triangle.points[0].w, triangle.texcoords[0].u, triangle.texcoords[0].v,
+                triangle.points[1].x, triangle.points[1].y, triangle.points[1].z, triangle.points[1].w, triangle.texcoords[1].u, triangle.texcoords[1].v,
+                triangle.points[2].x, triangle.points[2].y, triangle.points[2].z, triangle.points[2].w, triangle.texcoords[2].u, triangle.texcoords[2].v,
+                mesh_texture
+            );
+        }
+
         // Draw unfilled triangles -- Wireframe
-        if (render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX || render_method == RENDER_FILL_TRIANGLE_WIRE){
+        if (render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX || render_method == RENDER_FILL_TRIANGLE_WIRE || render_method == RENDER_TEXTURED_WIRE){
             draw_triangle(
                 triangle.points[0].x, triangle.points[0].y,
                 triangle.points[1].x, triangle.points[1].y,
@@ -254,8 +293,10 @@ void render(void){
 // Free memory that was dynamically allocated by the program
 void free_resources(void){
     free(color_buffer);
+    upng_free(png_texture);
     array_free(mesh.faces);
     array_free(mesh.vertices);
+
 }
 
 int main(void){
--- src/triangle.c
+++ src/triangle.c
@@ -1,12 +1,8 @@
 #include <stdint.h>
 #include "display.h"
+#include "swap.h"
 #include "triangle.h"
 
-void int_swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
 // Flat Bottom Triangle
 //        (x0, y0)
@@ -97,5 +93,171 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
     }   
 }
 
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
+}
 
-// TODO: Create Implementation for triangle.h functions
+vec3_t barycentric_weights(vec2_t a, vec2_t b, vec2_t c, vec2_t p){
+    // Find the vectors between the vertices ABC and point p
+    vec2_t ac = vec2_sub(c, a);
+    vec2_t ab = vec2_sub(b, a);
+    vec2_t pc = vec2_sub(c, p);
+    vec2_t pb = vec2_sub(b, p);
+    vec2_t ap = vec2_sub(p, a);
+
+    // Area of the full parallelogram (triangle ABC) using cross product
+    float area_parallelogram_abc = (ac.x * ab.y - ac.y * ab.x);
+
+    // Alpha = area of parallelogram PBC / area of full paralellogram ABC
+    float alpha = (pc.x * pb.y - pc.y * pb.x) / area_parallelogram_abc;
+
+    // Beta = area of parallelogram PBC / area of full paralellogram ABC
+    float beta = (ac.x * ap.y - ac.y * ap.x) / area_parallelogram_abc;
+
+    // Since alpha + beta + gamma = 1;
+    float gamma = 1.0 - alpha - beta;
+
+    vec3_t weights = { alpha, beta, gamma};
+    return weights;
+}
+
+// Function to draw the textured pixel at position x and y using interpolation
+void draw_texel(
+    int x, int y, uint32_t *texture, 
+    vec4_t point_a, vec4_t point_b, vec4_t point_c,
+    tex2_t a_uv, tex2_t b_uv, tex2_t c_uv
+){  
+    vec2_t p = {x, y};
+    vec2_t a = vec2_from_vec4(point_a);
+    vec2_t b = vec2_from_vec4(point_b);
+    vec2_t c = vec2_from_vec4(point_c);
+
+
+    vec3_t weights = barycentric_weights(a, b, c, p);
+
+    float alpha = weights.x;
+    float beta = weights.y;
+    float gamma = weights.z;
+
+    // Variables to store the interpolated values of U, V and also w for the current pixel
+    float interpolated_u;
+    float interpolated_v;
+    float interpolated_reciprocal_w;
+
+    // Perform the interpolation of all U/w, V/w values using barycentric weights and a factor of 1/w
+    interpolated_u = (a_uv.u / point_a.w) * alpha + (b_uv.u / point_b.w) * beta + (c_uv.u / point_c.w) * gamma;
+    interpolated_v = (a_uv.v / point_a.w) * alpha + (b_uv.v / point_b.w) * beta + (c_uv.v / point_c.w) * gamma;
+
+    // Also interpolate the value of 1/w for the current pixel
+    interpolated_reciprocal_w = (1/ point_a.w) * alpha + (1 / point_b.w) * beta + (1 / point_c.w) * gamma;
+
+    // Now we can divide back both interpolated values by 1/w
+    interpolated_u /= interpolated_reciprocal_w;
+    interpolated_v /= interpolated_reciprocal_w;
+
+    // Map the UV coordinates to the full texture width and height
+    int tex_x = abs((int)(interpolated_u * texture_width));
+    int tex_y = abs((int)(interpolated_v * texture_height));
+
+    // Check tex_x, tex_y boundings less than, greater than
+    int index = (texture_width * tex_y) + tex_x;
+    draw_pixel(x, y, texture[index] );
+}
+    
+
+// Drawing a textured triangle with flat-top/flat-bottom method
+
+void draw_textured_triangle(
+    int x0, int y0, float z0, float w0,float u0, float v0,
+    int x1, int y1, float z1, float w1, float u1, float v1,
+    int x2, int y2, float z2, float w2, float u2, float v2,
+    uint32_t* texture
+){ 
+    // Sort the vertices by y-coordinate ascending (y0 < y1 < y2)
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+        float_swap(&z0, &z1);
+        float_swap(&w0, &w1);
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+    if (y1 > y2){
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+        float_swap(&z1, &z2);
+        float_swap(&w1, &w2);
+        float_swap(&u1, &u2);
+        float_swap(&v1, &v2);
+    }
+    if (y0 > y1){
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+        float_swap(&z0, &z1);
+        float_swap(&w0, &w1);
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+
+    // Create vector points after we sort the vertices
+    vec4_t point_a = {x0, y0, z0, w0};
+    vec4_t point_b = {x1, y1, z1, w1};
+    vec4_t point_c = {x2, y2, z2, w2};
+    tex2_t a_uv = {u0, v0};
+    tex2_t b_uv = {u1, v1};
+    tex2_t c_uv = {u2, v2};
+
+    // Render upper-part (flat-bottom)
+    float inv_slope1 = 0;
+    float inv_slope2 = 0;
+
+    if (y1 - y0 != 0) inv_slope1 = (float)( x1 - x0 ) / abs( y1 - y0 );
+    if (y2 - y0 != 0) inv_slope2 = (float)( x2 - x0 ) / abs( y2 - y0 );
+
+    if (y1 - y0 != 0) {
+        for( int y = y0; y <= y1; y++ ) {
+            int x_start = x1 + (y - y1) * (inv_slope1);
+            int x_end = x0 + (y - y0) * (inv_slope2);
+
+            if(x_end < x_start) {
+                int_swap(&x_start, &x_end);
+            }   
+
+            for(int x = x_start; x < x_end; x++){
+                // TODO:
+                // Draw our pixel with the color that comes from the texture
+                draw_texel(x, y, texture, point_a, point_b, point_c, a_uv, b_uv, c_uv);
+            }
+        }
+    }
+
+
+    // Render bottom-part (flat-top)
+
+    inv_slope1 = 0;
+    inv_slope2 = 0;
+
+    if (y2 - y1 != 0) inv_slope1 = (float)( x2 - x1 ) / abs( y2 - y1 );
+    if (y2 - y0 != 0) inv_slope2 = (float)( x2 - x0 ) / abs( y2 - y0 );
+
+    if (y2 - y1 != 0) {
+        for( int y = y1; y <= y2; y++ ) {
+            int x_start = x1 + (y - y1) * (inv_slope1);
+            int x_end = x0 + (y - y0) * (inv_slope2);
+
+            if(x_end < x_start) {
+                int_swap(&x_start, &x_end);
+            }   
+
+            for(int x = x_start; x < x_end; x++){
+                // TODO:
+                // Draw our pixel with the color that comes from the texture
+                draw_texel(x, y, texture, point_a, point_b, point_c, a_uv, b_uv, c_uv);
+            }
+        }
+    }
+
+}
+
